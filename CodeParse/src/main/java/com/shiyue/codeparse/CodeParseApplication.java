c358b95c51ff4a17f0c2d6622aadc3cb94b24b71
王辉 <2422645737@qq.com>
2025-02-09 19:10:59 +0800

    创建项目接结构

---
+++ CodeParse/src/main/java/com/shiyue/codeparse/CodeParseApplication.java
@@ -0,0 +1,13 @@
+package com.shiyue.codeparse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeParseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeParseApplication.class, args);
+	}
+
+}
